363f3567d8ade1f40c991e75e27ac45a2b43b83a
Yegor Yakovishen <yegor.yakovishen@me.com>
2016-08-21 13:41:10 +0300

    Приведены в порядок папки AJAX, google-auth и google-signup

--- day4/google-signup/js/utils.js
+++ day4/google-signup/js/utils.js
@@ -1,4 +1,12 @@
+// validateEmail('user@example.com') => true
+// validateEmail('user123456') => false
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
+}
+
+// validatePhone('+79161234567') => true
+// validatePhone('84950000') => false
+function validatePhone(phone) {
+    return /^\+[0-9]+$/.test(phone);
 }
